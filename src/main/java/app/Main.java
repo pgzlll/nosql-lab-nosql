73575f538302d623bfe60f7515020708be85ceb7
Pınar <118536467+pgzlll@users.noreply.github.com>
2025-07-10 21:16:32 +0300

    Main.java güncellendi

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,10 +2,14 @@
 package app;
 
 import static spark.Spark.*;
+
+import app.model.Student;
 import com.google.gson.Gson;
 import app.store.*;
+import app.util.StudentGenerator;
 
 public class Main {
+
     public static void main(String[] args) {
         port(8080);
         Gson gson = new Gson();
@@ -13,6 +17,13 @@ public class Main {
         RedisStore.init();
         HazelcastStore.init();
         MongoStore.init();
+
+        for (int i = 1; i<=10000; i++){
+            Student s = StudentGenerator.generate(i);
+            RedisStore.put(s.getOgrenciNo(),s);
+            HazelcastStore.put(s.getOgrenciNo(),s);
+            MongoStore.put(s.getOgrenciNo(),s);
+        }
 
         get("/nosql-lab-rd/ogrenci_no=:id", (req, res) ->
             gson.toJson(RedisStore.get(req.params(":id"))));
@@ -22,5 +33,6 @@ public class Main {
 
         get("/nosql-lab-mon/ogrenci_no=:id", (req, res) ->
             gson.toJson(MongoStore.get(req.params(":id"))));
+
     }
 }
