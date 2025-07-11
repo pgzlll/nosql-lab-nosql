3674eb48fbee89b8a6f9b9f0b85528be6edab348
Pınar <118536467+pgzlll@users.noreply.github.com>
2025-07-11 12:23:48 +0300

    Main.java' ya performTest metodu eklendi

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -6,7 +6,7 @@ import static spark.Spark.*;
 import app.model.Student;
 import com.google.gson.Gson;
 import app.store.*;
-import app.util.StudentGenerator;
+import app.StudentGenerator;
 
 public class Main {
 
@@ -18,21 +18,56 @@ public class Main {
         HazelcastStore.init();
         MongoStore.init();
 
-        for (int i = 1; i<=10000; i++){
+        for (int i = 1; i <= 10000; i++) {
             Student s = StudentGenerator.generate(i);
-            RedisStore.put(s.getOgrenciNo(),s);
-            HazelcastStore.put(s.getOgrenciNo(),s);
-            MongoStore.put(s.getOgrenciNo(),s);
+            RedisStore.put(s.getOgrenciNo(), s);
+            HazelcastStore.put(s.getOgrenciNo(), s);
+            MongoStore.put(s.getOgrenciNo(), s);
         }
 
-        get("/nosql-lab-rd/ogrenci_no=:id", (req, res) ->
-            gson.toJson(RedisStore.get(req.params(":id"))));
+        get("/nosql-lab-rd/:id", (req, res) ->
+                gson.toJson(RedisStore.get(req.params(":id"))));
 
-        get("/nosql-lab-hz/ogrenci_no=:id", (req, res) ->
-            gson.toJson(HazelcastStore.get(req.params(":id"))));
+        get("/nosql-lab-hz/:id", (req, res) ->
+                gson.toJson(HazelcastStore.get(req.params(":id"))));
 
-        get("/nosql-lab-mon/ogrenci_no=:id", (req, res) ->
-            gson.toJson(MongoStore.get(req.params(":id"))));
+        get("/nosql-lab-mon/:id", (req, res) ->
+                gson.toJson(MongoStore.get(req.params(":id"))));
 
+        get("/perfomance", (req, res) -> {
+            res.type("text/plain");
+            return performTest();
+        });
+    }
+    public static String performTest(){
+        long start, end;
+        StringBuilder sb = new StringBuilder();
+
+        // redis
+        start = System.nanoTime();
+        for (int i=1; i<=10000; i++){
+            String id = "2025" + String.format("%06d", i);
+            RedisStore.get(id);
+        }
+        end = System.nanoTime();
+        sb.append("Redis 10.000 sorgu suresi: ").append((end-start)/1_000_000).append("ms\n");
+
+        // mongodb
+        for (int i=1; i<=10000; i++){
+            String id = "2025" + String.format("%06d", i);
+            MongoStore.get(id);
+        }
+        end = System.nanoTime();
+        sb.append("MongoDB 10.000 sorgu suresi: ").append((end-start)/1_000_000).append("ms\n");
+
+        // hazelcast
+        for(int i=1; i<=10000; i++){
+            String id = "2025" + String.format("%06d", i);
+            HazelcastStore.get(id);
+        }
+        end = System.nanoTime();
+        sb.append("Hazelcast 10.000 sorgu suresi: ").append((end-start)/1_000_000).append("ms");
+
+        return sb.toString();
     }
 }
